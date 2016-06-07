8b55d5fa75f3a6601a7cfc6ed1e9646606f94755
Rock Neurotiko <miguelglafuente@gmail.com>
2016-06-07 08:48:39 +0200

    .el base

---
+++ java-mode-for-real.el
@@ -0,0 +1,36 @@
+;;; java-mode-for-real.el --- A real mode for java language.
+
+;; Copyright (C) 2016 Java In Emacs Team
+;;
+;; Version: 0.0.1
+;; Keywords: java mode programming
+;; Author: Java In Emacs Team (ACM UPM members: Samuel, Pablo, Luis, Ignacio, Cesar, Adrian, Roberto, Miguel)
+;; URL: http://github.com/javainemacs/java-mode-for-real
+;; Package-Requires: ((emacs "24.1"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(message "Jaba Reglas")
+
+(provide 'java-mode-for-real)
+;;; java-mode-for-real.el ends here
